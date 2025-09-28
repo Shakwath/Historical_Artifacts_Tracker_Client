ae079a2f502a535c17ce0b181f19ac810e882410
shakwath <shakaawathhossain0@gmail.com>
2025-09-28 19:27:36 +0600

    login part done

--- src/Components/router/router.jsx
+++ src/Components/router/router.jsx
@@ -1,7 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
 import Mainlayout from "../MainLayout/MainLayout";
 import Home from "../Pages/Home";
-
+import Login from "../Authentication/Login";
+import AllArtifacts from "../Pages/AddArtifact"
+import AddArtifact from "../Pages/AddArtifact";
+import MyArtifacts from "../Pages/MyArtifacts";
 
 export const router = createBrowserRouter ([
   {
@@ -11,6 +14,22 @@ export const router = createBrowserRouter ([
       {
         path: "/",
         element:<Home></Home>,
+      },
+      {
+        path: "/allartifacts",
+        element:<AllArtifacts></AllArtifacts>
+      },
+      {
+        path: "/addartifacts",
+        element:<AddArtifact></AddArtifact>
+      },
+      {
+        path: "/myartifacts",
+        element:<MyArtifacts></MyArtifacts>
+      },
+      {
+        path: "/login",
+        element:<Login></Login>
       },
       //  {
       //   path: "/*",
